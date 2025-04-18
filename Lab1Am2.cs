25ae67515610594ebaac5bcfdcb14df258cd34a8
xoma69 <121461172+xoma69@users.noreply.github.com>
2025-04-18 10:32:46 +0300

    Lab1_Zavd2
    =0

---
+++ Lab1Am2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 3, 6, 9, 12, 15, 18, 21 };
+        int count = 0;
+
+        foreach (int num in numbers)
+        {
+            if (num % 2 != 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"Кількість непарних чисел: {count}");
+    }
+}
