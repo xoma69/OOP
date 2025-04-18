ce5a5fe806a72a9ec38194585812cae4f0cba4cc
xoma69 <121461172+xoma69@users.noreply.github.com>
2025-04-18 10:35:48 +0300

    Lab1Am_zavd3

---
+++ Lab1Am3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    enum Season { Зима, Весна, Літо, Осінь }
+
+    static void Main()
+    {
+        Console.Write("Введіть номер місяця (1–12): ");
+        int month = int.Parse(Console.ReadLine());
+        Season season;
+
+        if (month == 12 || month == 1 || month == 2)
+            season = Season.Зима;
+        else if (month >= 3 && month <= 5)
+            season = Season.Весна;
+        else if (month >= 6 && month <= 8)
+            season = Season.Літо;
+        else if (month >= 9 && month <= 11)
+            season = Season.Осінь;
+        else
+        {
+            Console.WriteLine("Невірний номер місяця.");
+            return;
+        }
+
+        Console.WriteLine($"Пора року: {season}");
+    }
+}
