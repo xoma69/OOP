e23cae1d21218094b0ca920b209df9f95d3ed366
xoma69 <121461172+xoma69@users.noreply.github.com>
2025-05-05 21:47:09 +0300

    Add files via upload

---
+++ Lab_4/4-1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+class Point
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    public Point(double x = 0, double y = 0)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+class Circle
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius = 1.0)
+    {
+        Radius = radius;
+    }
+
+    public double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+
+    public double GetCircumference()
+    {
+        return 2 * Math.PI * Radius;
+    }
+}
+
+class Round
+{
+    private Point Center;
+    private Circle Circle;
+
+    public Round(Point center, double radius)
+    {
+        Center = center;
+        Circle = new Circle(radius);
+    }
+
+    public void SetRadius(double radius)
+    {
+        Circle.Radius = radius;
+    }
+
+    public bool ContainsPoint(Point p)
+    {
+        double dx = p.X - Center.X;
+        double dy = p.Y - Center.Y;
+        double distanceSquared = dx * dx + dy * dy;
+        return distanceSquared <= Circle.Radius * Circle.Radius;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Центр: ({Center.X}, {Center.Y}), Радіус: {Circle.Radius}");
+        Console.WriteLine($"Площа: {Circle.GetArea():F2}");
+        Console.WriteLine($"Довжина кола: {Circle.GetCircumference():F2}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Round myRound = new Round(new Point(0, 0), 5);
+        myRound.PrintInfo();
+
+        Point testPoint = new Point(3, 4);
+        Console.WriteLine($"Точка (3,4) належить колу: {myRound.ContainsPoint(testPoint)}");
+    }
+}
---
+++ Lab_4/4-2.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+abstract class TPrism
+{
+    public double Height { get; protected set; }
+    public double BaseArea { get; protected set; }
+
+    public TPrism(double baseArea, double height)
+    {
+        BaseArea = baseArea;
+        Height = height;
+    }
+
+    public abstract double GetVolume();
+    public abstract double GetSurfaceArea();
+}
+
+class TPrism3 : TPrism
+{
+    public TPrism3(double baseArea, double height) : base(baseArea, height) { }
+
+    public override double GetVolume()
+    {
+        return BaseArea * Height;
+    }
+
+    public override double GetSurfaceArea()
+    {
+        return 3 * Height * Math.Sqrt(BaseArea) + 2 * BaseArea;
+    }
+}
+
+class TPrism4 : TPrism
+{
+    public TPrism4(double baseArea, double height) : base(baseArea, height) { }
+
+    public override double GetVolume()
+    {
+        return BaseArea * Height;
+    }
+
+    public override double GetSurfaceArea()
+    {
+        return 4 * Height * Math.Sqrt(BaseArea) + 2 * BaseArea;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть площу основи трикутної призми: ");
+        double base3 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введіть висоту трикутної призми: ");
+        double h3 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть площу основи чотирикутної призми: ");
+        double base4 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введіть висоту чотирикутної призми: ");
+        double h4 = Convert.ToDouble(Console.ReadLine());
+
+        int m = 4;
+        List<TPrism> prisms = new List<TPrism>();
+
+        double volume = new TPrism3(base3, h3).GetVolume();
+        for (int i = 0; i < m; i++)
+        {
+            if (i % 2 == 0)
+            {
+                prisms.Add(new TPrism3(base3, volume / base3));
+            }
+            else
+            {
+                prisms.Add(new TPrism4(base4, volume / base4));
+            }
+            volume += 5;
+        }
+
+        double triangleVolumeSum = 0;
+        double quadSurfaceSum = 0;
+
+        foreach (var p in prisms)
+        {
+            if (p is TPrism3)
+                triangleVolumeSum += p.GetVolume();
+            else if (p is TPrism4)
+                quadSurfaceSum += p.GetSurfaceArea();
+        }
+
+        Console.WriteLine($"\nСума об'ємів трикутних призм: {triangleVolumeSum:F2}");
+        Console.WriteLine($"Сума площ поверхонь чотирикутних призм: {quadSurfaceSum:F2}");
+    }
+}
