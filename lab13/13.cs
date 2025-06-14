777bd5ea6a4c3d86096a57b1c72f02dd978e5c43
xoma69 <121461172+xoma69@users.noreply.github.com>
2025-06-14 17:26:00 +0300

    Add files via upload

---
+++ lab13/13.cs
@@ -0,0 +1,36 @@
+﻿private Dictionary<string, string> _cache;
+
+private DataCache()
+{
+    _cache = new Dictionary<string, string>();
+}
+
+public static DataCache Instance
+{
+    get
+    {
+        lock (_lock)
+        {
+            return _instance ??= new DataCache();
+        }
+    }
+}
+
+public void Add(string key, string value)
+{
+    _cache[key] = value;
+}
+
+public string Get(string key)
+{
+    return _cache.TryGetValue(key, out string value) ? value : "[Not Found]";
+}
+
+public void PrintAll()
+{
+    Console.WriteLine("Поточний вміст кешу:");
+    foreach (var pair in _cache)
+    {
+        Console.WriteLine($"{pair.Key}: {pair.Value}");
+    }
+}
